38b4aa96af8908a8d51c8ff1e750891fc4b27ee9
ProRichyMan <prorichyman@gmail.com>
2022-09-05 19:42:20 -0700

    Temporary fix to navbar

--- src/main/java/com/hacks/Calculator/CalculatorMath.java
+++ src/main/java/com/hacks/Calculator/CalculatorMath.java
@@ -1,32 +1,32 @@
-// package com.hacks.Calculator;
+package com.hacks.Calculator;
 
-// import org.jfree.data.function.Function2D;
+import org.jfree.data.function.Function2D;
 
-// public class CalculatorMath {
-//     private double num1;
-//     private double num2;
-//     private String funct;
+public class CalculatorMath {
+    private double num1;
+    private double num2;
+    private String funct;
 
-//     public CalculatorMath(double num1, double num2, String funct) {
-//         this.num1 = num1;
-//         this.num2 = num2;
-//         this.funct = funct;
+    public CalculatorMath(double num1, double num2, String funct) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.funct = funct;
 
-//         this.calculateOutput();
-//     }
+        this.calculateOutput();
+    }
 
-//     public double getNum1() {
-//         return this.num1;
-//     }
+    public double getNum1() {
+        return this.num1;
+    }
 
-//     public double getNum2() {
-//         return this.num2;
-//     }
+    public double getNum2() {
+        return this.num2;
+    }
 
-//     public String getFunct() {
-//         return this.funct;
-//     }
+    public String getFunct() {
+        return this.funct;
+    }
 
     
 
-// }
+}
